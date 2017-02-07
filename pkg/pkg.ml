1874081e6009c9e440ab62951e1447d6dda26548
Valentin Reis <valentin.reis@gmail.com>
2017-02-07 13:17:59 +0100

    pkg change

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -5,7 +5,7 @@ open Topkg
 
 let () =
   Pkg.describe 
-    ~distrib:(Pkg.distrib ~uri:"http://freux.fr/obandit/distrib/" ()) "obandit" 
+    ~distrib:(Pkg.distrib ()) "obandit" 
   @@ fun c ->
   Ok [ Pkg.mllib "src/obandit.mllib";
        Pkg.test "test/test"; ]
