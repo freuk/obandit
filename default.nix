0b7615c11aee323cbd3a44db899eea721379f3c8
Valentin Reis <valentin.reis@gmail.com>
2017-04-04 21:44:39 +0200

    add packaging.

--- default.nix
+++ default.nix
@@ -1,15 +1,21 @@
-with import <nixpkgs> {};
-{
-  topkgEnv = stdenv.mkDerivation {
+{pkgs ? import <nixpkgs> {} }:
+let
+  callPackage = pkgs.lib.callPackageWith (pkgs // pkgs.xlibs // self);
+  #ocamlCallPackage = pkgs.ocamlPackages.callPackageWith (pkgs // pkgs.xlibs // self);
+  self = rec {
+    obandit = pkgs.ocamlPackages.callPackage ./obandit.nix { };
+    dev = pkgs.stdenv.mkDerivation {
     name = "baseOcaml";
     buildInputs =
-    [ bzip2
-    gmp
-    (texlive.combine {
-    inherit (texlive) cases scheme-small;
+    [ pkgs.bzip2
+    pkgs.gmp
+    (pkgs.texlive.combine {
+    inherit (pkgs.texlive) cases scheme-small;
     })
   ];
     TOPKG_DELEGATE=''./ocaml_delegate.ml'';
-    shellHook = '' export PS1="topkg-prompt> " '';
+    #shellHook = '' export PS1="topkg-prompt> " '';
   };
-}
+  };
+in
+  self
