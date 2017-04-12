0fd10494258da9cd2dbb5758ccbc5d16cc40ce76
Valentin Reis <valentin.reis@gmail.com>
2017-04-12 22:18:36 +0200

    successfully added custom generator to the topkg pipeline using an ocamlbuild custom rule

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,25 +1,19 @@
 open Ocamlbuild_plugin
-open Ocamlbuild_pack
 
+let () =
+  dispatch (function
+    | After_options ->
+       let ocamldoc tags deps docout docdir = 
+         let tags = tags -- "extension:html" in 
+         Ocamlbuild_pack.Ocaml_tools.ocamldoc_l_dir tags deps docout docdir in 
 
-(* calls out to Ocaml_tools.ocamldoc_l_dir but filters out the
-"extension:html"
-flag which causes -html to be added to the ocamldoc command line.*)
-let my_ocamldoc tags deps docout docdir =
-let tags = tags -- "extension:html" in
-Ocaml_tools.ocamldoc_l_dir tags deps docout docdir
-
-(* copy of the normal ocamldoc rule that uses our custom generator. *)
-let () = Rule.rule
-"ocamldoc: document ocaml project odocl & *odoc -> docdir (html+)"
-~insert:`top
-~prod:"%.docdir/index.html"
-~stamp:"%.docdir/html.stamp"
-~dep:"%.odocl"
-(Ocaml_tools.document_ocaml_project
-~ocamldoc:my_ocamldoc
-"%.odocl" "%.docdir/index.html" "%.docdir");
-
-dep ["doc";"ocaml";"intro"] ["lib/intro.txt"];
-flag ["doc";"ocaml"] & S[A"-i";A"-g";A"_build/odoc_custom.cma"];
-flag ["doc";"ocaml";"intro"] & S[A"-intro";A"lib/intro.txt"]
+       rule "ocamldoc with plugin" 
+         ~prod:"%.docdir/index.html" 
+         ~stamp:"%.docdir/html.stamp" 
+         ~deps:["%.odocl"; "doc/plugin.cmxs"]
+         (Ocamlbuild_pack.Ocaml_tools.document_ocaml_project 
+            ~ocamldoc "%.odocl" "%.docdir/index.html" "%.docdir"); 
+      
+      pflag ["ocaml"; "doc"; "docdir"] "plugin"
+        (fun plugin -> S [A "-g"; A plugin])
+    | _ -> ())
