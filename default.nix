c0a7433cfe1e41388a76785ac93406ad2605922a
Valentin Reis <valentin.reis@gmail.com>
2017-07-25 13:58:40 +0200

    default.nix fix

--- default.nix
+++ default.nix
@@ -3,6 +3,7 @@ let
   callPackage = pkgs.lib.callPackageWith (pkgs // pkgs.xlibs // self);
   #ocamlCallPackage = pkgs.ocamlPackages.callPackageWith (pkgs // pkgs.xlibs // self);
   self = rec {
-    obandit = pkgs.ocamlPackages.callPackage ./obandit.nix { };
-n
+    obandit = pkgs.ocamlPackages.callPackage ./obandit.nix { src = ./.;};
+  };
+in
   self
