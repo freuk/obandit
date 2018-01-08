a8a88cccb65afe36969fe32c378b51e126623e5d
Valentin Reis <valentin.reis@gmail.com>
2018-01-08 08:54:41 +0100

    obandit ref for build fix

--- obandit.nix
+++ obandit.nix
@@ -3,10 +3,13 @@
 stdenv.mkDerivation rec {
 	name = "obandit-${version}";
 	version = "0.2.1";
-	src = fetchurl {
-		url = "https://github.com/freuk/obandit/archive/v0.2.1-zenodo.tar.gz";
-		sha256 = "1a8l4kvv811vy11gjj9nvz1px7s4zxrr3zi53zblvkibq56zzl7s";
-	};
+
+  src = ./.;
+
+	#src = fetchurl {
+		#url = "https://github.com/freuk/obandit/archive/v0.2.1-zenodo.tar.gz";
+		#sha256 = "1a8l4kvv811vy11gjj9nvz1px7s4zxrr3zi53zblvkibq56zzl7s";
+	#};
 
 	unpackCmd = "tar xjf $src";
 
