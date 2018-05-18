45cb27600f8b45001f43ad65505ea2bae95177f7
Valentin Reis <valentin.reis@gmail.com>
2018-05-18 11:16:31 +0200

    plugin fix

--- doc/plugin.ml
+++ doc/plugin.ml
@@ -1,4 +1,3 @@
-(** MathJax header *)
 let mathjax_header =
   "<script type=\"text/x-mathjax-config\">\n\
    MathJax.Hub.Config({\n\
@@ -8,13 +7,7 @@ let mathjax_header =
    },\
    TeX:{\
      Macros: {\
-       C: '{\\\\mathbb C}',\
-       R: '{\\\\mathbb R}',\
-       Q: '{\\\\mathbb Q}',\
-       Z: '{\\\\mathbb Z}',\
-       bm: ['{\\\\boldsymbol #1}', 1],\
        argmax: '\\\\mathop{\\\\rm arg\\\\,max}\\\\limits',\
-       argmin: '\\\\mathop{\\\\rm arg\\\\,min}\\\\limits'\
      }\
    }\
    });\n\
@@ -26,24 +19,21 @@ let mathjax_header =
 
 let add_mathjax_to_header header b ?nav ?comments t =
   let b' = Buffer.create 64 in
-  header b' ?nav ?comments t; (* Generate the original header. *)
-  let eoh = "</head>\n" in (* The string at the end of a header. *)
+  header b' ?nav ?comments t;
+  let eoh = "</head>\n" in
   Buffer.add_substring b (Buffer.contents b') 0
-    (Buffer.length b' - String.length eoh); (* Remove [eoh]. *)
+    (Buffer.length b' - String.length eoh);
   Buffer.add_string b mathjax_header;
-  Buffer.add_string b eoh (* Append [eoh] *)
+  Buffer.add_string b eoh
 
 module Generator (G : Odoc_html.Html_generator) =
 struct
   class html =
     object(self)
       inherit G.html as super
-
       method prepare_header module_list =
-        print_endline "*** Using mathjax custom ocamldoc generator.";
         super#prepare_header module_list; (* destructively modifies [header] *)
         header <- add_mathjax_to_header header
-
       method html_of_Latex buffer code = Buffer.add_string buffer code
     end
 end
